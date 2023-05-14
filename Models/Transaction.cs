9fff9758ebcb516fc2e1f87ecfa058aa27b4b312
sharath-98 <sharathchander.p@gmail.com>
2023-05-14 11:50:45 -0500

    Validation for Transaction

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -10,10 +10,11 @@ namespace ExpenseTracker.Models
         public int TransactionId { get; set; }
 
         // Foreign key specification
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
 
-
+        [Range(1, int.MaxValue, ErrorMessage ="Amount should be greater than 0.")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
