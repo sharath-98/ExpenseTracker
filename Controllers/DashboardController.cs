97c8288cf976ade5bd8bf919da70c4d5f2a173ce
sharath-98 <sharathchander.p@gmail.com>
2023-05-14 15:10:56 -0500

    Three Carts for expense, income and balance

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.EJ2.PivotView;
 
 namespace ExpenseTracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        //Last 7 days transacations
+        DateTime startDate = DateTime.Today.AddDays(-6);
+        DateTime endDate = DateTime.Today;
+
+        public async Task<ActionResult> Index()
+        {
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= startDate && y.Date <= endDate)
+                .ToListAsync();
+
+            //total income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(x => x.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString();
+
+            // Total Expense
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(x => x.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString();
+
+            // Balance
+            int Balance = TotalIncome - TotalExpense;
+            ViewBag.Balance = Balance.ToString();
+
             return View();
         }
     }
